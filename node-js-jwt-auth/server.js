46db7ec3548c552b7bb00569174ce667f165ce3e
Ludin-Najera <69707485+Ludin-Najera@users.noreply.github.com>
2020-09-29 22:50:49 -0600

    reconfiguracion

--- node-js-jwt-auth/server.js
+++ node-js-jwt-auth/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
